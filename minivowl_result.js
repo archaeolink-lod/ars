d423ef16e7083e6afc41efdac26190711e3c5805
florianthiery <mail@fthiery.de>
2023-12-04 12:11:35 +0000

    Deploying to gh-pages from @ archaeolink-lod/ars@761b02e4ff4eacae2bf78a41898aae0da99cd005 🚀

---
+++ minivowl_result.js
@@ -0,0 +1,47 @@
+var minivowlresult={
+ "info": [
+  {
+   "description": "Created with pyowl2vowl (version 0.1) as part of the SPARQLing Unicorn QGIS Plugin"
+  }
+ ],
+ "nodes": [
+  {
+   "name": "Feature",
+   "type": "class",
+   "uri": "http://archaeology.link/ontology#Feature"
+  },
+  {
+   "name": "Polygon",
+   "type": "class",
+   "uri": "http://www.opengis.net/ont/sf#Polygon"
+  },
+  {
+   "name": "Activity",
+   "type": "class",
+   "uri": "http://www.w3.org/ns/prov#Activity"
+  }
+ ],
+ "links": [
+  {
+   "source": 0,
+   "target": 1,
+   "valueTo": "hasGeometry",
+   "propertyTo": "class",
+   "uriTo": "http://www.opengis.net/ont/geosparql#hasGeometry"
+  },
+  {
+   "source": 0,
+   "target": 2,
+   "valueTo": "wasGeneratedBy",
+   "propertyTo": "class",
+   "uriTo": "http://www.w3.org/ns/prov#wasGeneratedBy"
+  },
+  {
+   "source": 0,
+   "target": 1,
+   "valueTo": "selectedArea",
+   "propertyTo": "class",
+   "uriTo": "http://archaeology.link/ontology#selectedArea"
+  }
+ ]
+}
